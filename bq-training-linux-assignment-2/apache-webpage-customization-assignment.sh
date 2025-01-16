6d34f1df24f0360ea5ee94ec7536f6b9ddfc634f
Mann Mittal <mann.mittal@betaque.com>
2025-01-16 16:19:21 +0530

    Updated: apache-webpage-customization-assignment

--- bq-training-linux-assignment-2/apache-webpage-customization-assignment.sh
+++ bq-training-linux-assignment-2/apache-webpage-customization-assignment.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Part 1
 
 #install and configure Apache2
@@ -9,7 +8,7 @@ echo "$pass" | sudo -S apt install apache2
 #sudo launchctl load -w /System/Library/LaunchDaemons/org.apache.httpd.plist
 
 #customize the Apache service default webpage
-cat '<<EOF > /var/www/html/index.html    
+echo "$pass" | sudo -S bash -c 'cat <<EOF > /var/www/html/index.html                                      
 <html><body>
 <h1>Welcome to the advanced Linux training module</h1>
 <p>Learning by doing is the key to mastery.</p>
@@ -35,21 +34,23 @@ sudo systemctl start firewalld
 sudo systemctl status firewalld
 sudo firewall-cmd --permanent --add-port=80/tcp
 sudo firewall-cmd --reload
-sudo -4 ifconfig.me
+address=$(curl -4 ifconfig.me)
 
 # test public ip using curl
-curl 54.90.166.9:80
+curl $address
 
 #Create new file name script.sh for Timestamp
-cat '<<EOF > /Users/mac/script.sh
+touch script.sh
+path=$(pwd script.sh)
+bash -c 'cat <<EOF > /$path/script.sh
 echo "$(date) <br>" >> /var/www/html/index.html
 EOF'
 
 # Update Timestamp after 2 minutes using crontab
-(crontab -l 2>/dev/null; echo "*/2 * * * * /Users/mac/script.sh") | crontab -
+(crontab -l 2>/dev/null; echo "*/2 * * * * /h$path/script.sh") | crontab -
 
 # test using curl
-curl 54.90.166.9:80
+curl $address
 
 # Using browser 
 # http://54.90.166.9.80
