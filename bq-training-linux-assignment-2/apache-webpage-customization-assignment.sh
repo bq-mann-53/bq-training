fa62cebd953e2b3d6ebc27fb6939380282911752
Mann Mittal <mann.mittal@betaque.com>
2025-01-16 12:10:51 +0530

    Updated: apache-webpage-customization-assignment

--- bq-training-linux-assignment-2/apache-webpage-customization-assignment.sh
+++ bq-training-linux-assignment-2/apache-webpage-customization-assignment.sh
@@ -3,13 +3,13 @@
 #Part 1
 
 #install and configure Apache2
-brew install apache2
+echo "$pass" | sudo -S apt install apache2
 
 #start apache server and work after system reboot
 #sudo launchctl load -w /System/Library/LaunchDaemons/org.apache.httpd.plist
 
 #customize the Apache service default webpage
-echo "betaque@123" | sudo -S bash -c 'cat <<EOF > /opt/homebrew/var/www/index.html    #Default location of file for port 8080
+echo "$pass" | sudo -S bash -c 'cat <<EOF > /opt/homebrew/var/www/index.html    #Default location of file for port 8080
 <html><body>
 <h1>Welcome to the advanced Linux training module</h1>
 <p>Learning by doing is the key to mastery.</p>
@@ -28,44 +28,28 @@ curl http://localhost:80
 
 #Part 2
 
-#Configure Apache Server with Ngrok
-brew install ngrok
-ngrok config add-authtoken 2rcg5aQQRstBVuEZ3diT6ZIrxVQ_6fHeG1hfDwmQG3QpnNFe3    #for authentication
-ngrok http http://localhost:80              #to make app online by replacing local ip with public ip
+#Configure Apache Server using firewall in ubuntu
 
-#Send public ip to team member for verification
-# https://0448-2409-40c4-3010-ae44-978-f6ab-d0c8-edc9.ngrok-free.app
+echo "$pass" | sudo -S apt install firewalld
+sudo systemctl start firewalld
+sudo systemctl status firewalld
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --reload
+sudo -4 ifconfig.me
 
-# Second way
-#find public ip address using whatismyip website
-# when we write public id to browser it will open a new admin page from where we do port forwarding
-# Last we use that ip so open file to other users.
+# test public ip using curl
+curl 54.90.166.9:80
 
-#Third way using firewall in ubuntu
-# sudo apt install firewalld
-# sudo systemctl start firewalld
-# sudo systemctl status firewalld
-# sudo firewall-cmd --permanent --add-port=80/tcp
-# sudo firewall-cmd --reload
-# sudo -4 ifconfig.me
+#Create new file name script.sh for Timestamp
+cat <<EOF > /Users/mac/script.sh
+echo "$(date) <br>" >> /opt/homebrew/var/www/index.html
+EOF
+
+# Update Timestamp after 2 minutes using crontab
+(crontab -l 2>/dev/null; echo "*/2 * * * * /Users/mac/script.sh") | crontab -
 
 # test using curl
-# curl 54.90.166.9:80
+curl 54.90.166.9:80
 
-#Update timestamp in 2 minutes using crontab
-#crontab -e
-#i
-#*/2 * * * * /Users/mac/script.sh
-#esc
-#ZZ
-
-# script.sh file
-#cat <<EOF echo "$(date) <br>" >> /opt/homebrew/var/www/index.html
-#EOF
-
-
-#Verify using public ip
-#https://0448-2409-40c4-3010-ae44-978-f6ab-d0c8-edc9.ngrok-free.app
-
-#using curl
-curl https://0448-2409-40c4-3010-ae44-978-f6ab-d0c8-edc9.ngrok-free.app
+# Using browser 
+# http://54.90.166.9.80
